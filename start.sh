d6153c9457eb259cab1197a4e40449ade265c6dd
sudarshan <sdevkota007@gmail.com>
2019-04-18 19:57:14 -0400

    modified readme

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 echo "Start to process the data......"
-
+#
 #use this to parse tcp header files
 #time python pcap_Parser.py -p > "parser.log"
 # OR
 #use this to parse http header files
 time python pcap_Parser.py > "parser.log"
-
-
+###
 time python load_shift.py
