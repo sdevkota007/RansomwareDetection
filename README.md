48b424486b9d4bcef8b07987dcb32980192819cf
sudarshan <sdevkota007@gmail.com>
2019-04-18 19:25:42 -0400

    download bug fixed

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 Ransomware Detection using Deep Learning
 
 
+Download the malware and normal dataset using
+```bash
+cd downloads
+python wgetall.py
+```
+
+Copy around 90% of the downloaded malware dataset into ./malware_pcap/train_source/ and remaining 10% to 
+./malware_pcap/test_source/
+
+Simarly, Copy around 90% of the downloaded normal dataset into ./normal_pcap/train_source/ and remaining 10% to 
+./normal_pcap/test_source/
+
 unzip all files in a directory with password
 ```
 unzip -P <password> \*.zip
--- downloads/wgetall.py
+++ downloads/wgetall.py
@@ -1,5 +1,8 @@
 import subprocess
+import os
 
+malware_folder = "./malware_data"
+normal_folder = "./normal_data"
 
 with open('malware_dataset.txt', 'r') as f:
     malware_dataset = f.readlines()
@@ -7,14 +10,19 @@ with open('malware_dataset.txt', 'r') as f:
 with open('normal_dataset.txt', 'r') as f:
     normal_dataset = f.readlines()
 
+if not os.path.isdir(malware_folder):
+        os.mkdir(malware_folder)
+
+if not os.path.isdir(normal_folder):
+        os.mkdir(normal_folder)
 
 for url in malware_dataset:
     if url.startswith("http"):
         url = url.strip('\n')
-        subprocess.call(["wget", "-r", "-nc", "-l 1", url])
+        subprocess.call(["wget","-P", malware_folder, "-nc", "-l 1", url])
 
 
 for url in normal_dataset:
     if url.startswith("http"):
         url = url.strip('\n')
-        subprocess.call(["wget", "-r", "-nc", "-l 1", url])
+        subprocess.call(["wget","-P", normal_folder, "-nc", "-l 1", url])
