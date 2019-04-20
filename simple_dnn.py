58006cbf9e43e574a1eb775b92c32b132207016e
sudarshan <sdevkota007@gmail.com>
2019-04-19 23:30:01 -0400

    new version started

--- simple_dnn.py
+++ simple_dnn.py
@@ -5,9 +5,6 @@ import pickle
 from sklearn.utils import shuffle
 import numpy as np
 
-# mnist = tf.keras.datasets.mnist  # mnist is a dataset of 28x28 images of handwritten digits and their labels
-# (x_train, y_train),(x_test, y_test) = mnist.load_data()  # unpacks images to x_train/x_test and labels to y_train/y_test
-
 
 
 with open("train.pickle", "rb") as f:
@@ -18,21 +15,24 @@ with open("train.pickle", "rb") as f:
 x_train, y_train = get_seperate(shuffle(zip(reduced, Ytrain)))
 
 
-with open("test.pickle", "rb") as f:
-    reduced = pickle.load(f)
-    Xtest = pickle.load(f)
-    Ytest = pickle.load(f)
-
-x_test, y_test = get_seperate(shuffle(zip(reduced, Ytest)))
-
-
+#convert training set to numpy array
 x_train = np.array(x_train, dtype=np.float32)
 x_train = x_train.reshape((x_train.shape[0], -1, x_train.shape[1]))
-x_test = np.array(x_test, dtype=np.float32)
-x_test = x_test.reshape((x_test.shape[0], -1, x_test.shape[1]))
-
 y_train = np.array(y_train, dtype=np.float32)
-y_test = np.array(y_test, dtype=np.float32)
+
+
+#normalize, scales data between 0 and 1
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+
+
+#prepare validation set from training set
+validation_size = int(x_train.shape[0] * 0.15)  # percent of training features to use as validation set; 0 to disable validation set
+x_validation = x_train[:validation_size]
+y_validation = y_train[:validation_size]
+x_train = x_train[validation_size:]
+y_train = y_train[validation_size:]
+
+
 
 
 print('x_train')
@@ -40,17 +40,6 @@ print('Shape', x_train.shape)
 print('Type', type(x_train))
 print('Type of one training example', type(x_train[0]))
 
-
-print('x_test')
-print('Shape', x_test.shape)
-print('Type', type(x_test))
-print('Type of one training example', type(x_test[0]))
-exit()
-
-
-
-x_train = tf.keras.utils.normalize(x_train, axis=1)  # scales data between 0 and 1
-x_test = tf.keras.utils.normalize(x_test, axis=1)  # scales data between 0 and 1
 
 model = tf.keras.models.Sequential()  # a basic feed-forward model
 model.add(tf.keras.layers.Flatten())  # takes our 28x28 and makes it 1x784
@@ -64,6 +53,6 @@ model.compile(optimizer='adam',  # Good default optimizer to start with
 
 model.fit(x_train, y_train, epochs=3)  # train the model
 
-val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
+val_loss, val_acc = model.evaluate(x_validation, y_validation)  # evaluate the out of sample data with model
 print(val_loss)  # model's loss (error)
 print(val_acc)  # model's accuracy
