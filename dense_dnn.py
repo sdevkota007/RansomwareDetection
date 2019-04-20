eccf9422c4ce892001d8e68b0a5d43da20693406
sudarshan <sdevkota007@gmail.com>
2019-04-20 17:57:54 -0400

    confusion matrix, fp_rate, fn_rate added

--- dense_dnn.py
+++ dense_dnn.py
@@ -1,13 +1,24 @@
+from __future__ import division
 import tensorflow as tf
 import sys
-from util import get_seperate
+from util import get_seperate, plot_confusion_matrix, compute_fp_fn_rate
 import pickle
 from sklearn.utils import shuffle
 import numpy as np
 from collections import Counter
 import tensorboard
+import time
+from tensorflow.keras.callbacks import TensorBoard
 
 
+if len(sys.argv) > 1:
+    detail = sys.argv[1]
+
+else:
+    detail = int(time.time())
+
+NAME = 'dense_dnn-{}'.format(detail)
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 with open("train.pickle", "rb") as f:
     reduced = pickle.load(f)
@@ -24,7 +35,7 @@ y_train = np.array(y_train, dtype=np.float32)
 
 
 #normalize, scales data between 0 and 1
-x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
 
 num_training_examples = Counter(y_train).values()
 print("No. of training examples")
@@ -43,18 +54,30 @@ y_train = y_train[validation_size:]
 
 model = tf.keras.models.Sequential()  # a basic feed-forward model
 model.add(tf.keras.layers.Flatten())  # takes our 28x28 and makes it 1x784
-model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))  # a simple fully-connected layer
-model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))  # a simple fully-connected layer
-model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Dropout(0.6))
+model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))  # a simple fully-connected layer
+model.add(tf.keras.layers.Dropout(0.6))
 model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))  # our output layer. 2 units for 2 classes. Softmax for probability distribution
 
 model.compile(optimizer='adam',  # Good default optimizer to start with
               loss='sparse_categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
               metrics=['accuracy'])  # what to track
 
-model.fit(x_train, y_train, batch_size=32, epochs=3)  # train the model
+model.fit(x_train, y_train,
+          batch_size=32,
+          epochs=8,
+          callbacks=[tensorboard])  # train the model
 
 val_loss, val_acc = model.evaluate(x_validation, y_validation)  # evaluate the out of sample data with model
-print(val_loss)  # model's loss (error)
-print(val_acc)  # model's accuracy
+predictions = model.predict_classes(x_validation, batch_size=10, verbose=0)
+
+print("Validation Loss: ", val_loss)  # model's loss (error)
+print("Validation Accuracy: ",val_acc)  # model's accuracy
+
+# Plot confusion Matrix and calculate false-pos rate and false-neg rate
+Labels = ['Not-Ransomware', 'Ransomware']
+_,cm = plot_confusion_matrix(y_validation, predictions,classes=Labels, title='Confusion Matrix', plot = False)
+fp_rate, fn_rate = compute_fp_fn_rate(cm)
+print("False Positive rate: {}".format(fp_rate))
+print("False Negative rate: {}".format(fn_rate))
--- util.py
+++ util.py
@@ -1,4 +1,9 @@
+from __future__ import division
 import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+import itertools
+import matplotlib.pyplot as plt
 
 
 def get_seperate(train):
@@ -43,3 +48,71 @@ def infoDisplay(counter, ipcounter, tcpcounter, udpcounter, httpcounter):
     print "Total number of tcp packets: ", tcpcounter
     print "Total number of udp packets: ", udpcounter
     print "Total number if http requests: ", httpcounter
+
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues,
+                          plot = True):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    if plot:
+        plt.show()
+    return ax, cm
+
+
+def compute_fp_fn_rate(cm):
+    tp = cm[0][0]
+    fp = cm[0][1]
+    fn = cm[1][0]
+    tn = cm[1][1]
+
+    fp_rate = fp/(tn+fp)
+    fn_rate = fn/(tp+fn)
+    return fp_rate, fn_rate
